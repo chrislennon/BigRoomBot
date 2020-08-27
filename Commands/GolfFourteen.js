391c446d83cef5e12c9d3eaab8497441b51e5065
Chris Lennon <chrislennon@users.noreply.github.com>
2020-08-27 13:26:00 +0200

    Add 14 and no14 commands

---
+++ Commands/GolfFourteen.js
@@ -0,0 +1,41 @@
+const AWS = require('aws-sdk');
+const Command = require(`./Command.js`);
+
+class GolfFourteen extends Command {
+  constructor() {
+    super();
+    this.name = `14`;
+    this.aliases = [`14`, `fourteen`, `fail`];
+    this.description = `Increments number of 14 scores on a user.`;
+    this.usage = `14 [@user]`;
+  }
+
+  execute(Message) {
+    var dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+    const targetUser = Message.mentions.users.size > 0 ? Message.mentions.users.first() : Message.author;
+
+    var params = {
+      TableName: process.env.DYNAMO_TABLE_NAME,
+      Key: { "id": targetUser.id },
+      ExpressionAttributeValues: { ":inc": 1 },
+      ExpressionAttributeNames: {
+        "#field": "golfFourteens"
+      },
+      UpdateExpression: "ADD #field :inc",
+      ReturnValues: "UPDATED_NEW"
+    };
+    
+    var numberWins = dynamoDB.update(params, function(err, data) {
+      if (err) {
+          console.log(err, err.stack);
+      } else {
+          console.log(data);
+      }
+    });
+
+    Message.reply(`${targetUser.toString()} has ${numberWins} lucky 14s!`);
+  }
+}
+
+module.exports = GolfFourteen;
---
+++ Commands/GolfUnFourteen.js
@@ -0,0 +1,41 @@
+const AWS = require('aws-sdk');
+const Command = require(`./Command.js`);
+
+class GolfUnFourteen extends Command {
+  constructor() {
+    super();
+    this.name = `no14`;
+    this.aliases = [`no14`, `nofourteen`, `nofail`];
+    this.description = `Removes a 14 score on a user.`;
+    this.usage = `no14 [@user]`;
+  }
+
+  execute(Message) {
+    var dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+    const targetUser = Message.mentions.users.size > 0 ? Message.mentions.users.first() : Message.author;
+
+    var params = {
+      TableName: process.env.DYNAMO_TABLE_NAME,
+      Key: { "id": targetUser.id },
+      ExpressionAttributeValues: { ":inc": -1 },
+      ExpressionAttributeNames: {
+        "#field": "golfFourteens"
+      },
+      UpdateExpression: "ADD #field :inc",
+      ReturnValues: "UPDATED_NEW"
+    };
+    
+    var numberWins = dynamoDB.update(params, function(err, data) {
+      if (err) {
+          console.log(err, err.stack);
+      } else {
+          console.log(data);
+      }
+    });
+
+    Message.reply(`${targetUser.toString()} has ${numberWins} lucky 14s!`);
+  }
+}
+
+module.exports = GolfUnFourteen;
